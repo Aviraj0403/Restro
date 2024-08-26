9e4ce12243a5132035e7d5622f582e89952e41d7
aviraj0403 <aviraj0403@gmail.com>
2024-08-26 07:59:43 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/TD').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DataBase Connected"));
 }
